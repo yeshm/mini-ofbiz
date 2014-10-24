d00bdfe88e32bc0f861cb306076f6e786708014a
yeshm <yeshuangming@gmail.com>
2014-10-24 15:54:07 +0800

    添加代码演练场 1.http://localhost:8080/example/control/testGroovy 2.http://localhost:8080/example/control/testJson

---
+++ hot-deploy/example/script/org/miniofbiz/example/TestEvents.groovy
@@ -0,0 +1,41 @@
+package org.miniofbiz.example
+
+import org.ofbiz.base.util.UtilHttp
+import org.ofbiz.entity.Delegator
+import org.ofbiz.entity.GenericValue
+import org.ofbiz.service.LocalDispatcher
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+public String testGroovy() {
+    HttpServletRequest request = request
+    HttpServletResponse response = response
+    Delegator delegator = delegator
+    LocalDispatcher dispatcher = dispatcher
+    def requestParameters = UtilHttp.getParameterMap(request)
+
+    PrintWriter writer = response.getWriter()
+
+    //演练代码开始
+
+
+    writer.write("hello world!")
+    writer.close()
+
+    return "error"
+}
+
+public String testJson() {
+    HttpServletRequest request = request
+    Delegator delegator = delegator
+    LocalDispatcher dispatcher = dispatcher
+    GenericValue userLogin = userLogin
+    def requestParameters = UtilHttp.getParameterMap(request)
+
+    //演练代码开始
+
+    request.setAttribute("message", "hello world!")
+
+    return "success"
+}
