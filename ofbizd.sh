54ec6a02026d5f99871f982503262b7162d0723d
yeshm <yeshuangming@gmail.com>
2016-09-13 11:40:20 +0800

    ofbizd.sh优化

--- ofbizd.sh
+++ ofbizd.sh
@@ -1,70 +1,68 @@
 #!/bin/sh
 
 PROJECT_HOME=`pwd`
+echo "PROJECT_HOME:$PROJECT_HOME"
 
 if [ ! -d "$PROJECT_HOME/hot-deploy" ];then
-  echo "$PROJECT_HOME/hot-deploy 该目录不存在，请在ofbiz项目目录中执行$0, 后缀带sh的，请加上x权限"
-  exit
+    echo "$PROJECT_HOME/hot-deploy 该目录不存在，请在ofbiz项目目录中执行$0, 后缀带sh的，请加上x权限"
+    exit
 fi
 
 case "$1" in
-   start)
-
-	pids=`ps aux|grep java|grep "ofbiz.jar"|awk '{print $2}'`
+    start)
+        pids=`ps aux|grep ofbiz|grep "$PROJECT_HOME/tools/[s]tartofbiz.sh"|awk '{print $2}'`
         if [ -n "$pids" ]; then
-                echo "ofbiz server is running"
-		exit
+            echo "ofbiz server is running"
+            exit
         else
-	       echo "ofbiz server is start..."
-	       ulimit -SHn 65535
-               $PROJECT_HOME/tools/startofbiz.sh 1>/dev/null 2>$PROJECT_HOME/tools/startofbiz.log &
-	       sleep 10
-	       $PROJECT_HOME/$0 status
+	        echo "ofbiz server is start..."
+	        ulimit -SHn 65535
+            $PROJECT_HOME/tools/[s]tartofbiz.sh 1>/dev/null 2>$PROJECT_HOME/runtime/logs/startofbiz.log &
         fi
-   ;;
-   stop)
-            
-           pids=`ps aux|grep java|grep "ofbiz.jar"|awk '{print $2}'`
-             if [ -n "$pids" ]; then
-                    echo "执行ofbiz自带的stopofbiz.sh"
-                   $PROJECT_HOME/tools/stopofbiz.sh &
-             fi              
-            closed=0
-            sleep_time=1
-             while [ $closed -eq 0 ]
-              do
-                  pids=`ps aux|grep java|grep "ofbiz.jar"|awk '{print $2}'`
-                  if [ -n "$pids" ]; then
-                       echo "等待ofbiz结束:$sleep_time,10秒后将强制关闭"
-                       sleep_time=$((sleep_time+1))                        
-                       if [ $sleep_time -gt 10 ];then
-                           kill -9 $pids
-                           echo "超过10秒，强制kill -9 ofbiz"                            
-                           closed=1
-                       else
-                           sleep 1
-                       fi
-                  else
-                      closed=1
-                  fi                  
-              done
-	     $PROJECT_HOME/$0 status
-   ;;
-   restart)
-       echo "暂时不用，因未写成service服务"
-       # $PROJECT_HOME/$0 stop
-       # $PROJECT_HOME/$0 start
-   ;;
-   status)
-	pids=`ps aux | grep java| grep "ofbiz.jar" | grep -v grep | awk '{print $2}'`
+    ;;
+    debug)
+        pids=`ps aux|grep ofbiz|grep "$PROJECT_HOME/tools/[s]tartofbiz.sh"|awk '{print $2}'`
         if [ -n "$pids" ]; then
-                echo "ofbiz server is running"
+            echo "ofbiz server is running"
+		    exit
         else
-                echo "ofbiz server is stop"
+            echo "ofbiz server is start debug..."
+	        ulimit -SHn 65535
+            $PROJECT_HOME/ant start-debug 1>/dev/null 2>$PROJECT_HOME/runtime/logs/startofbiz.log &
+	        sleep 10
+	        $PROJECT_HOME/$0 status
         fi
-   ;;
-   *)
-        echo $"Usage: $0 {start|stop|restart|status}"
+    ;;
+    stop)
+        pids=`ps aux|grep ofbiz|grep "$PROJECT_HOME/tools/[s]tartofbiz.sh"|awk '{print $2}'`
+            if [ -n "$pids" ]; then
+                echo "run stopofbiz.sh"
+                $PROJECT_HOME/tools/stopofbiz.sh &
+            else
+                echo "ofbiz server isn't running"
+            fi
+    ;;
+    log)
+        tail -100f $PROJECT_HOME/runtime/logs/ofbiz.log
+    ;;
+    error)
+        tail -100f $PROJECT_HOME/runtime/logs/error.log
+    ;;
+    restart)
+        echo "暂时不用，因未写成service服务"
+        # $PROJECT_HOME/$0 stop
+        # $PROJECT_HOME/$0 start
+    ;;
+    status)
+	    pids=`ps aux|grep ofbiz|grep "$PROJECT_HOME/tools/[s]tartofbiz.sh"|awk '{print $2}'`
+        if [ -n "$pids" ]; then
+            echo "ofbiz server is running"
+        else
+            echo "ofbiz server is stop"
+        fi
+    ;;
+    *)
+        echo $"Usage: $0 {start|stop|debug|status|log|error}"
         exit $?
-   ;;
+    ;;
 esac
