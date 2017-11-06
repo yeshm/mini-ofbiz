82e935f37f5bd81ed359773a669ebd937fe221e6
yeshm <yeshm@juweitu.com>
2017-11-06 10:16:56 +0800

    加速tomcat启动

--- tools/startofbiz.sh
+++ tools/startofbiz.sh
@@ -36,7 +36,7 @@ rm -f $OFBIZ_LOG
 MEMIF="-Xms128M -Xmx512M -XX:MaxPermSize=512m"
 #JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=33333 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 #MISC="-Duser.language=en"
-VMARGS="$MEMIF $MISC $JMX $DEBUG $RMIIF -Dfile.encoding=UTF-8"
+VMARGS="$MEMIF $MISC $JMX $DEBUG $RMIIF -Dfile.encoding=UTF-8 -Djava.security.egd=file:/dev/./urandom"
 
 # Worldpay Config
 #VMARGS="-Xbootclasspath/p:applications/accounting/lib/cryptix.jar $VMARGS"
